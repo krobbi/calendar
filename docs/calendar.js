37d0bab00f9dc7d247d93fc6ae19fe3f2ccb92f1
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-10-19 19:42:43 +0100

    Get month lengths

--- docs/calendar.js
+++ docs/calendar.js
@@ -1,30 +1,65 @@
+// Create an enumeration.
+function Enum(...names) {
+	const result = {
+		count: names.length,
+		names: Object.freeze(names),
+	};
+	
+	for (const value in names) {
+		result[names[value]] = parseInt(value);
+	}
+	
+	return Object.freeze(result);
+}
+
+const Month = Enum(
+	"JAN",
+	"FEB",
+	"MAR",
+	"APR",
+	"MAY",
+	"JUN",
+	"JUL",
+	"AUG",
+	"SEP",
+	"OCT",
+	"NOV",
+	"DEC",
+);
+
 // Return whether a year is a leap year.
 function isYearLeap(year) {
 	return year % 400 === 0 || year % 4 === 0 && year % 100 !== 0;
 }
 
-// Test a leap year.
-function testLeapYear(year, expected) {
-	const isLeapYear = isYearLeap(year);
-	
-	if (isLeapYear) {
-		console.debug(`${year} is a leap year.`);
-	} else {
-		console.debug(`${year} is not a leap year.`);
+// Get the length of a month in a year.
+function getMonthLength(year, month) {
+	switch (month) {
+		case Month.APR:
+		case Month.JUN:
+		case Month.SEP:
+		case Month.NOV:
+			return 30;
+		case Month.FEB:
+			return isYearLeap(year) ? 29 : 28;
+		default:
+			return 31;
 	}
+}
+
+// Test month lengths for a year.
+function testMonthLengths(year) {
+	console.debug(`In ${year}:`);
 	
-	if (isLeapYear !== expected) {
-		console.error("Test failed.");
+	for (let i = 0; i < Month.count; i++) {
+		console.debug(` * ${Month.names[i]} was ${getMonthLength(year, i)} days long.`);
 	}
 }
 
 // Main function.
 function main() {
-	testLeapYear(1600, true);
-	testLeapYear(1900, false);
-	testLeapYear(2000, true);
-	testLeapYear(2010, false);
-	testLeapYear(2012, true);
+	testMonthLengths(1900);
+	testMonthLengths(2000);
 }
 
 // Call the main function when ready.
